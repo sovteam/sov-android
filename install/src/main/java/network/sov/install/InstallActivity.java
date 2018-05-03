325d80003fb49d8abc59de4d8076f3d330b5c797
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-05-03 13:50:41 -0300

    Instant App Spike

--- install/src/main/java/network/sov/install/InstallActivity.java
+++ install/src/main/java/network/sov/install/InstallActivity.java
@@ -1,7 +1,14 @@
 package network.sov.install;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.instantapps.InstantApps;
 
 public class InstallActivity extends AppCompatActivity {
 
@@ -9,5 +16,39 @@ public class InstallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_install);
+
+        System.out.println(">>>>> CREATE");
+        ((TextView)findViewById(R.id.hello_label)).setText("Hello " + getIntent().getDataString() + System.currentTimeMillis());
+
+        init();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        System.out.println(">>>>> NEW INTENT");
+        init();
+    }
+
+    private void init() {
+
+        View installButton = findViewById(R.id.install_button);
+        if (InstantApps.isInstantApp(this))
+            initInstantApp(installButton);
+        else
+            installButton.setVisibility(View.GONE);
+    }
+
+    private void initInstantApp(View installButton) {
+        System.out.println(">>>>> INIT");
+
+        installButton.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View view) {
+            System.out.println(">>>>> BUTTON PRESSED");
+            Intent intent = new Intent(getApplicationContext(), InstallActivity.class);
+
+//            InstantApps.showInstallPrompt(InstallActivity.this, intent, 0, null);
+            startActivity(intent);
+        }});
     }
 }
