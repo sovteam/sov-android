58852c42beefd66cad55f76df36d9e8200bc2f03
Adalberto Soares <junynes@gmail.com>
2018-05-03 19:59:09 -0300

    Adds DummyActivity - WIP

---
+++ install/src/main/java/network/sov/install/DummyActivity.java
@@ -0,0 +1,12 @@
+package network.sov.install;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DummyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy);
+    }
+}
--- install/src/main/java/network/sov/install/InstallActivity.java
+++ install/src/main/java/network/sov/install/InstallActivity.java
@@ -1,10 +1,9 @@
 package network.sov.install;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,7 +17,25 @@ public class InstallActivity extends AppCompatActivity {
         setContentView(R.layout.activity_install);
 
         System.out.println(">>>>> CREATE");
+
+        String val = getIntent().getStringExtra("AppInstalled");
+        if (val != null) {
+            System.out.println(">>>>>> extra val: " + val);
+        } else {
+            System.out.println(">>>>>> extra val is null");
+        }
         ((TextView)findViewById(R.id.hello_label)).setText("Hello " + getIntent().getDataString() + System.currentTimeMillis());
+
+        findViewById(R.id.dummy_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setClassName("network.sov.app", DummyActivity.class.getName());
+                intent.putExtra("AppInstalled", "SOV");
+                startActivity(intent);
+            }
+        });
 
         init();
     }
@@ -44,10 +61,18 @@ public class InstallActivity extends AppCompatActivity {
         System.out.println(">>>>> INIT");
 
         installButton.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View view) {
-            System.out.println(">>>>> BUTTON PRESSED");
-            Intent intent = new Intent(getApplicationContext(), InstallActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            InstantApps.showInstallPrompt(InstallActivity.this, intent, 0, null);
+            System.out.println(">>>>> BUTTON PRESSED: " + getIntent().getDataString());
+            // Intent intent = new Intent(InstallActivity.this, DummyActivity.class);
+
+            final Intent intent = new Intent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setClassName("network.sov.app", DummyActivity.class.getName());
+            intent.putExtra("AppInstalled", "SOV");
+
+            /*Bundle bundle = new Bundle();
+            bundle.putString("AppInstalled", "SOV");*/
+
+            InstantApps.showInstallPrompt(InstallActivity.this, intent, 7, "InstallActivity");
         }});
     }
 }
