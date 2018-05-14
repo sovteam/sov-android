0d7e232979949c3596e2627069996d2c4a01636d
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-05-14 17:52:27 -0300

    Testing post-install intent.

--- install/src/main/java/network/sov/install/DummyActivity.java
+++ install/src/main/java/network/sov/install/DummyActivity.java
@@ -2,11 +2,14 @@ package network.sov.install;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DummyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy);
+
+        ((TextView)findViewById(R.id.dummyTextView)).setText(getIntent().getStringExtra("AppInstalled"));
     }
 }
